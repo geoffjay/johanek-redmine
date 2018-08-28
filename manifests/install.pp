fba387d678950cc046bc94bff8f21211570c1ceb
Geoff Johnson <geoff.johnson@coanda.ca>
2018-08-27 18:06:59 -0700

    Change build dependency for Debian Stretch

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,8 +4,24 @@ class redmine::install {
   # Install dependencies
 
   $generic_packages = [ 'make', 'gcc' ]
-  $debian_packages  = [ 'libmysql++-dev', 'libmysqlclient-dev', 'libmagickcore-dev', 'libmagickwand-dev', 'ruby-dev', 'libpq-dev', 'imagemagick' ]
-  $redhat_packages  = [ 'postgresql-devel', 'sqlite-devel', 'ImageMagick-devel', 'ruby-devel', $::redmine::params::mysql_devel ]
+  # TODO: Check for Debian release and use libmariadbclient-dev-compat instead of libmysqlclient-dev
+  $debian_packages  = [
+    'libmysql++-dev',
+    #'libmysqlclient-dev',
+    'libmariadbclient-dev-compat',
+    'libmagickcore-dev',
+    'libmagickwand-dev',
+    'ruby-dev',
+    'libpq-dev',
+    'imagemagick'
+  ]
+  $redhat_packages  = [
+    'postgresql-devel',
+    'sqlite-devel',
+    'ImageMagick-devel',
+    'ruby-devel',
+    $::redmine::params::mysql_devel
+  ]
 
   case $::osfamily {
     'Debian':   { $packages = concat($generic_packages, $debian_packages) }
